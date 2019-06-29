00ffb37130cc782bbec9ae199412e431e675252c
kendell <kendell.r@outlook.com>
2019-06-29 07:57:52 -0700

    Update __init__.py

--- cvFace/cvFace/__init__.py
+++ cvFace/cvFace/__init__.py
@@ -1,1 +1,26 @@
 import cv2
+from threading import Thread
+class smartFrameReader:
+  def __init__(self):
+    vidcap = cv2.VideoCapture(0)
+  def foreverRead(self):
+    """ don't worry about this. """
+    while True:
+      ret, tempFrame = self.vidcap.read()
+      if self.status != 0:
+        self.frame = tempFrame
+        self.status = 0
+  def getFrame(self):
+    """ gets frame from frame reader. """
+    self.status = 1
+    while self.status == 1:
+      pass
+    return self.frame
+  def startReading(self):
+    """ starts reading frames. i consistently read frames to remove lag. """
+    rdr = Thread(target=self.foreverRead)
+    rdr.start()
+class faceFinder:
+  pass
+class faceIDer:
+  pass
